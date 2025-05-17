57a6eb8357c358cd3eff78a596e5370addf5f9b9
abyss999 <abusivehydro@gmail.com>
2025-05-17 15:49:25 -0500

    Backend Start

---
+++ backend/app/routes/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask 
+from flask_cors import CORS
+from dotenv import load_dotenv
+import os 
+
+
+load_dotenv()
+
+def create_app():
+    app = Flask(__name__)
+    CORS(app)
+
+    from app.routes.discord import discord_bp
+    from app.routes.users import users_bp
+
+    app.register_blueprint(discord_bp, url_prefix='/discord')
+    app.register_blueprint(users_bp, url_prefix='/users')
+
+    return app
---
+++ backend/app/routes/discord.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, request, jsonify
+from app.db import connect
+
+discord_bp = Blueprint('discord', __name__)
+
+@discord_bp.route("/config/<string:guild_id>", methods=["GET"]):
+def getDiscordConfig(guild_id):
+    connection = connect()
+    with connect.cursor() as cur:
+        cur.execute(f"SELECT * FROM discord_config WHERE guild_id = %s", (guild_id, )) # we use %s to prevent SQL injection
+        result = cur.fetchone()
+        return jsonify(result) if result else jsonify({"error": "No config found"}), 404
