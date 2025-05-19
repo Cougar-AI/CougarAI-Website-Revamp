144d4390f2dcc9e77e4c048e8682c70c28377728
Abyss999 <abusivehydro@gmail.com>
2025-05-19 15:11:29 -0500

    backend mirgrate to gunicorn

--- backend/script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-source ../venv/bin/activate  # adjust if your venv is elsewhere
-export FLASK_APP=run.py
-export FLASK_ENV=production
-flask run --host=0.0.0.0 --port=5000
