6967649ca9e10c84e621fbf6227233caf5dfc4cd
Abyss999 <abusivehydro@gmail.com>
2025-05-19 15:28:45 -0500

    run.py update

--- backend/run.py
+++ backend/run.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 import psycopg2.extras
 import psycopg2
 from dotenv import load_dotenv
-from app import create_app
+from backend.app import create_app
 import os
 
 load_dotenv()
