f9beb7008a8327015a3a3aeb2dfc461004822749
Abyss999 <abusivehydro@gmail.com>
2025-05-20 20:25:30 -0500

    script.sh please work i am going crazy

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 source venv/bin/activate
-exec gunicorn app.wsgi:app --chdir backend --bind 0.0.0.0:5000 --workers 4
+exec gunicorn wsgi:app --chdir backend --bind 0.0.0.0:5000 --workers 4
