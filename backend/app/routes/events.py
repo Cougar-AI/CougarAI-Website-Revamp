c307cd7cf698199650ede5f74b6b740e05ec2b48
Abyss999 <abusivehydro@gmail.com>
2025-05-27 23:17:06 -0500

    date handler to date validation

--- backend/app/routes/events.py
+++ backend/app/routes/events.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from app.utils.date_handler import is_valid_date
+from app.utils.date_validation import is_valid_date
 from app.utils.query_handler import build_sql_querys
 from app.db import connect
 
