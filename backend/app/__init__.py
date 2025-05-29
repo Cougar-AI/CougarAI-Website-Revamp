a937884f197e09bc78a3293cbcbae0857407884b
Jose Conde <145078308+LemurTech22@users.noreply.github.com>
2025-05-29 15:28:11 -0500

    route clean up

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -4,13 +4,7 @@ def create_app():
     app = Flask(__name__)
     CORS(app)
 
-  
-    app.register_blueprint(discord_bp, url_prefix='/discord')
-    app.register_blueprint(users_bp, url_prefix='/users')
-    app.register_blueprint(payments_bp, url_prefix='/payments')
-    app.register_blueprint(events_bp, url_prefix='/events')
-    app.register_blueprint(officers_bp, url_prefix='/officers')
-    app.register_blueprint(points_bp, url_prefix='/points')
-    app.register_blueprint(auth_bp, url_prefix='/auth')
-
+    for blueprint, prefix in blueprints_with_prefixes.items():
+        app.register_blueprint(blueprint, url_prefix=prefix)
+        
     return app
